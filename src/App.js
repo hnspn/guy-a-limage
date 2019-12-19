95326425fb630b1a0a8bdcc75f6cce8e3e041044
Abderahmane Bouziane <bobmatt911@gmail.com>
2019-12-19 14:43:07 -0500

    Basic images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+
+function Image(props) {
+  const {src} = props;
+
+  return <img src={src} width={400} height={400} />;
+}
+
 function App() {
   return (
-    <div>
-			HELLO MARCUS
-    </div>
+    <>
+      <Image src="https://images.unsplash.com/photo-1533158307587-828f0a76ef46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"/>
+      <Image src="https://images.unsplash.com/photo-1455311683036-3e949a996256?ixlib=rb-1.2.1&auto=format&fit=crop&w=1605&q=80"/>
+    </>
   );
 }
 
